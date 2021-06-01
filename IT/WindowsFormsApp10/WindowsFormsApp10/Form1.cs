856c4b173ef604465de3e56fa3cfbf8b47066a62
mariabodrova00@mail.ru <mariabodrova00@mail.ru>
2021-06-01 20:54:32 +0300

    add

---
+++ IT/WindowsFormsApp10/WindowsFormsApp10/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp10
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+    //Вариант 3. Разработайте программу анимации двигающегося человечка. 
+    private void timer1_Tick(object sender, EventArgs e)
+    {
+      a += 5;
+      x1 += a;
+      x2 += a;
+      x3 += a;
+      x4 += a;
+      r1 += a;
+      z = a % 2;
+      if(z==0)
+      {
+        x1 += b;
+        x2 -= b;
+        x4 += b;
+        x3 -= b;
+      }
+      else
+      {
+        x2 += b;
+        x1 -= b;
+        x3 += b;
+        x4 -= b;
+      }
+      
+      Invalidate();
+    }
+    
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      x1 = 40; 
+      y1 = 60;
+      x2 = 90;
+      y2 = 160;
+      x3 = 90;
+      y3 = 60;
+      x4 = 40;
+      y4 = 160;
+      r1 = 40;
+      r2 = 60;
+      a = 0;
+      b = 10;
+
+    }
+    private int x1, y1, x2, y2, x3, y3, x4, y4, r1, r2; 
+    private int a, b;
+    private double z;
+    private Pen pen = new Pen(Color.Black, 3);
+    private SolidBrush brush1 = new SolidBrush(Color.Black);
+    private SolidBrush brush2 = new SolidBrush(Color.Green);
+    private void Form1_Paint(object sender, PaintEventArgs e)
+    {
+      Graphics g = e.Graphics; 
+      g.DrawLine(pen, x1, y1, x2, y2);
+      g.DrawLine(pen, x3, y3, x4, y4);
+      Rectangle rect = new Rectangle(r1+10, r2+10, 30, 80);
+      g.FillEllipse(brush2, rect);
+      Rectangle krug = new Rectangle(r1+10, r2-10, 30, 30);
+      g.FillEllipse(brush1, krug);
+    }
+  }
+}
---
+++ IT/WindowsFormsApp11/WindowsFormsApp11/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp11
+{
+  public partial class Form1 : Form
+  {
+    
+    private Bitmap bmp;
+    
+    private Graphics g;
+    public Form1()
+    {
+      InitializeComponent();
+    }
+    //Вариант 3. Создайте функцию, переводящую изображение в черно-белый формат.
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      
+    }
+    private void button1_Click(object sender, EventArgs e)
+    {
+      OpenFileDialog dialog = new OpenFileDialog();
+      dialog.Filter = "Image files (*.BMP, *.JPG, *.PNG)|*.png; *.jpg; *.bmp";
+      if (dialog.ShowDialog() == DialogResult.OK)
+      {
+        Image image = Image.FromFile(dialog.FileName);
+        int width = image.Width;
+        int height = image.Height;
+        pictureBox1.Width = width;
+        pictureBox1.Height = height;
+        bmp = new Bitmap(image, width, height);
+        pictureBox1.Image = bmp;
+        g = Graphics.FromImage(pictureBox1.Image);
+      }
+    }
+    private void button2_Click(object sender, EventArgs e)
+    {
+      SaveFileDialog savedialog = new SaveFileDialog();
+      savedialog.Title = "Сохранить картинку как...";
+      savedialog.OverwritePrompt = true;
+      savedialog.CheckPathExists = true;
+      savedialog.Filter=
+        "Bitmap File(*.bmp)|*.bmp|"+
+        "JPEG File(*.jpg)|*.jpg|" +
+        "PNG File(*.png)|*.png";
+      savedialog.ShowHelp = true;
+      if (savedialog.ShowDialog() == DialogResult.OK)
+      {
+        string fileName = savedialog.FileName;
+        string strFilExtn = fileName.Remove(0, fileName.Length - 3);
+        switch(strFilExtn)
+        {
+          case "bmp":
+            bmp.Save(fileName, System.Drawing.Imaging.ImageFormat.Bmp);
+            break;
+          case "jpg":
+            bmp.Save(fileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+            break;
+          case "png":
+            bmp.Save(fileName, System.Drawing.Imaging.ImageFormat.Png);
+            break;
+          default:
+            break;
+        }
+      }
+    }
+
+    private void button3_Click(object sender, EventArgs e)
+    {
+      for (int i = 0; i < bmp.Width; i++)
+        for (int j=0;j<bmp.Height;j++)
+        {
+          int R = bmp.GetPixel(i, j).R;
+          int G = bmp.GetPixel(i, j).G;
+          int B = bmp.GetPixel(i, j).B;
+          int Gray = (R +G + B) / 3;
+          Color p = Color.FromArgb(255, Gray, Gray, Gray);
+          bmp.SetPixel(i, j, p);
+        }
+      Refresh();
+    }
+
+  }
+}
